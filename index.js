d62bcf27a2ececddd260c9834341d01053e192c5
Gautam Dhakate <106535248+Gautam-121@users.noreply.github.com>
2023-08-20 17:21:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,12 +26,14 @@ async function generateToken(){
 
 app.get("/getAllData" ,async (req , res)=>{
 
+    const getToken = await generateToken()
+
     const data =  await axios({
         method: 'post',
         url: 'https://dev-test.cimet.io/plan-list',
         headers : {
             "Api-key" : "4NKQ3-815C2-8T5Q2-16318-55301",
-            "Auth-token" : "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcGlfa2V5IjoiNE5LUTMtODE1QzItOFQ1UTItMTYzMTgtNTUzMDEiLCJzdWIiOjQzOCwiaXNzIjoiaHR0cHM6Ly9kZXZjb3JlMDIuY2ltZXQuaW8vdjEvZ2VuZXJhdGUtdG9rZW4iLCJpYXQiOjE2OTI1MjEwMTMsImV4cCI6MTY5MjUzMTgxMywibmJmIjoxNjkyNTIxMDEzLCJqdGkiOiJIRWlKQlFYakQxWHpmR2ZNIn0.jtqkekTyPzZpfHGMYRxluqAKXrN-Pip9vSNR7W5PeWs"
+            "Auth-token" : getToken
         },
         data : {
             "session_id":"eyJpdiI6IkVNUkZ1N0hlSHhHSnJ3Vjl4aUlxc0E9PSIsInZhbHVlIjoieFlxa1wvVDYxQWl5U2pxMDFcL0R6ZVVvdEN6Mkk0R29TRDN3ZnN0U3VGcER0cEFMa2NVb0xNcDJudjlRTHRUbGJkIiwibWFjIjoiMTE0MmU0MGE5YmJhMzY4Nzc4MDExNmZkNTI1MjZhMGE3OTQyMDZmOTc1MTVmZDM1Mzc3ZmJmNjhmMzllOGYxYSJ9"
